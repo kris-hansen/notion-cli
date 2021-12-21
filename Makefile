d0d1f722795907f76a7d5c53dfd3a95c76631ba6
Josh Holbrook <jholbrook@eaze.com>
2021-12-20 21:49:00 -0500

    Add isort

--- Makefile
+++ Makefile
@@ -4,7 +4,7 @@ setup:
 	python3 -m venv ./venv && source ./venv/bin/activate && pip install pip --upgrade && pip install -r requirements.txt && pip install -e . || rm -r venv
 
 format:
-	source ./venv/bin/activate ** black .
+	source ./venv/bin/activate && isort . && black .
 
 build:
 	source ./venv/bin/activate && pyinstaller -n notion --onefile cli.py
--- setup.py
+++ setup.py
@@ -1,11 +1,8 @@
-from setuptools import find_packages, setup
+from setuptools import find_packages
+from setuptools import setup
 
 setup(
     packages=find_packages(),
     include_package_data=True,
-    entry_points=dict(
-        console_scripts=[
-            "notion=notioncli.cli:main"
-        ]
-    )
+    entry_points=dict(console_scripts=["notion=notioncli.cli:main"]),
 )
